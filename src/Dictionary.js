4ee85fc73b6978427e4ad35dd4343f74940967a6
martuman95 <73444049+martuman95@users.noreply.github.com>
2021-10-05 17:56:10 +0200

    added images

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,11 +2,13 @@ import React, {useState} from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Result from "./Result";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
     let [keyword, setKeyword] = useState(props.defaultKeyword);
     let [result, setResult] = useState(null);
     let [loaded, setLoaded] = useState(false);
+    let [photos, setPhotos] = useState(null);
 
 function handleKeyword(event){
 setKeyword(event.target.value);
@@ -16,10 +18,17 @@ function handleResponse(response){
     console.log(response.data[0]);
     setResult(response.data[0]);
 }
-
+function handlePexelsResponse(response){
+setPhotos(response.data.photos);
+}
 function search(){
    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
    axios.get(apiUrl).then(handleResponse);
+
+   let pexelsApiKey = "563492ad6f91700001000001470bf03f14d0442cae665aacc8c767d0";
+   let pexelsUrl= `https://api.pexels.com/v1/search?query=${keyword}&per_page=6`;
+let headers = {Authorization: `Bearer ${pexelsApiKey} ` };
+   axios.get(pexelsUrl, { headers: headers }).then(handlePexelsResponse);
 }
    function HandleSubmit(event){
        event.preventDefault();
@@ -41,6 +50,7 @@ if(loaded){
 </section>
 <section className="dictionary">
 <Result results={result}/>
+<Photos photos={photos}/>
 </section>
 
         </div>
---
+++ src/Photos.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Photo.css";
+
+export default function Photos(props){
+if (props.photos) {
+return(
+    <div className="Photos">
+            <div className="row">
+       {props.photos.map(function(photo, index){
+return(
+    <div className="col-4" key={index} >
+    < a href={photo.src.original} target="_blank">
+  <img src={photo.src.tiny} className="img-fluid"/>
+</a>
+</div>
+)
+       })} 
+       </div>
+    </div>
+)
+} else {
+    return null;
+}
+}
